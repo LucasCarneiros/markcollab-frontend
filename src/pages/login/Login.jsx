1d522d068fe04c2b6f843a589c61a82a4ff08034
RafaeleSilv <rafaele.00000847840@unicap.br>
2024-11-20 11:21:19 -0300

    conectando tela de login a tela home

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Login.css'; 
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className="login-container">
 
       <div className="login-blue-side">
-        <svg className="login-arrow" width="30" height="30" viewBox="0 0 59 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M54.918 43.0015L34.2157 23.7779L54.918 4.55435" stroke="white" stroke-width="5.51686" stroke-linecap="square"/>
-        <path d="M25.3359 43.0015L4.63364 23.7779L25.3359 4.55435" stroke="white" stroke-width="5.51686" stroke-linecap="square"/>
-        </svg>
+      <Link to="/" className="login-arrow" style={{ cursor: "pointer" }}>
+  <svg width="30" height="30" viewBox="0 0 59 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M54.918 43.0015L34.2157 23.7779L54.918 4.55435" stroke="white" stroke-width="5.51686" stroke-linecap="square"/>
+    <path d="M25.3359 43.0015L4.63364 23.7779L25.3359 4.55435" stroke="white" stroke-width="5.51686" stroke-linecap="square"/>
+  </svg>
+</Link>
+
       </div>
       <div className="login-white-side">
         <h2 className="login-title">Fazer Login</h2>
